ce91b5e132df6c27db1f21b1b780b13768d3656a
Fozilbek <fl.raimberdiyev@mail.ru>
2023-02-15 15:46:01 +0500

    just push

---
+++ functions/api.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv")
+const morgan = require("morgan");
+const serverless = require('serverless-http');
+
+//setting node environment variables
+
+if(process.env.NODE_ENV==="production") {
+  dotenv.config({path : ".env.production  "})
+} else {
+  dotenv.config({path : ".env"})
+}
+
+//connecting to database
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.DB_HOST, {})
+  .then(() => {
+    console.log("MongoDB ga ulanish muvaffaqqiyatli amalga oshirildi");
+  })
+  .catch((er) => {
+    console.log("MongoDB ga ulanishda xato ro'y berdi", er);
+  });
+
+//declaring app
+const app = express();
+
+//initial route
+app.use("/api", require("../routes/index"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json())
+// parse application/json
+app.use(bodyParser.json());
+
+//using morgan logger
+// app.use(morgan("tiny"));
+
+// listening port
+app.listen(3000, () => {
+  console.log("Server is listening in ",3000);
+});
+
+module.exports.handler = serverless(app)

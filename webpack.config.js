fd5a1eb9498d30a48429e4c8214311ee2a69a38a
Fozilbek <fl.raimberdiyev@mail.ru>
2023-05-11 20:24:42 +0500

    checking

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   target: 'node',
-  mode : "development",
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),

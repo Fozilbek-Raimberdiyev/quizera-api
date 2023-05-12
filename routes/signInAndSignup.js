1ccb196f1293b99eb5739c946cdfe02c310291b8
Fozilbek <fl.raimberdiyev@mail.ru>
2023-05-12 16:20:06 +0500

    correcting uploads

--- routes/signInAndSignup.js
+++ routes/signInAndSignup.js
@@ -7,7 +7,7 @@ const checkAuth = require("../middleware/auth");
 const multer =require("multer")
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, `public/uploads/`);
+    callback(null, `/public/uploads/`);
   },
   filename: function (req, file, callback) {
     const filename = file.originalname.split(" ").join("_")
